206c560b493a06452d16ec8b2058d752ccf13bfd
Sabari2810 <sabarivijay281099@gmail.com>
2021-01-25 15:35:17 +0530

    new file added

---
+++ UnitTesting/cap.py
@@ -0,0 +1,9 @@
+def cap_text(text):
+    '''
+        gets a input string and
+        returns a capitalized output string
+    '''
+    return text.capitalize()
+
+def text_cap(text):
+    return text.title()
